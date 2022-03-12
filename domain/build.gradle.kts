4a2303d251c39695755f502840a869878b5f01a5
tmdgusya <tmdgusya@naver.com>
2022-03-12 21:15:17 +0900

    flyway migration conf 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,9 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.flywaydb.flyway") version "7.8.2"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
 }
+
+apply(plugin = "kotlin")
+
+val compileKotlin: KotlinCompile by tasks
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
@@ -24,6 +29,18 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+flyway {
+    url = "jdbc:mysql://localhost:3306/roki?serverTimezone=UTC&characterEncoding=UTF-8"
+    user = "root"
+    locations = arrayOf("filesystem:${file("src/main/resources/db/migration/").absolutePath}")
+    password = "root"
+    encoding = "UTF-8"
+}
+
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
@@ -33,4 +50,20 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
+
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
 }
---
+++ domain/src/main/resources/db/migration/V120220312__init_schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `roki`.`users`
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    email      VARCHAR(100) NOT NULL,
+    auth_token VARCHAR(200) NOT NULL,
+    role       VARCHAR(10)  NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE `roki`.`post`
+(
+    id        BIGINT       NOT NULL AUTO_INCREMENT,
+    tile      VARCHAR(200) NOT NULL,
+    content   TEXT         NOT NULL,
+    version   INT          NOT NULL DEFAULT 1,
+    createdAt DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE `roki`.`contributes_logs`
+(
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    email   BIGINT       NOT NULL,
+    postId  BIGINT       NOT NULL,
+    postUrl VARCHAR(255) NOT NULL COMMENT 'Join 을 하지 않고 POST 를 보여주기 위한 용도',
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
