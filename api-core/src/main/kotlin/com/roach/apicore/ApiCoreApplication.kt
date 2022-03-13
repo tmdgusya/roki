30d3634adf172a5232dbb9f9fe4b9c62654efd89
tmdgusya <tmdgusya@naver.com>
2022-03-13 18:57:12 +0900

    api-core 모듈 추가

---
+++ api-core/src/main/kotlin/com/roach/apicore/ApiCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.roach.apicore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiCoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiCoreApplication>(*args)
+}
